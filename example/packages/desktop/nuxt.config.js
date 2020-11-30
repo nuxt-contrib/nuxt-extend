040443efeceb2fe0fdd3058dd1f610fd6b88286a
Pooya Parsa <pyapar@gmail.com>
2020-11-30 15:22:23 +0100

    chore: update example

--- example/packages/desktop/nuxt.config.js
+++ example/packages/desktop/nuxt.config.js
@@ -1,10 +1,5 @@
 import { nuxtConfig } from 'nuxt-extend'
 
-console.log(nuxtConfig({
-  name: '@app/desktop',
-  extends: '@app/base/nuxt.config'
-}))
-
 export default nuxtConfig({
   name: '@app/desktop',
   extends: '@app/base/nuxt.config'
